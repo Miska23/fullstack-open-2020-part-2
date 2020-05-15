520d22902b328e87893fa950b5b4ed5aaee0bee3
Miska23 <miskalinden@gmail.com>
2020-05-15 15:28:08 +0300

    removed console.log debugs

--- kurssitiedot/src/components/Course.js
+++ kurssitiedot/src/components/Course.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Course = ({ courses }) => {
     return (
       courses.map(course => 
-        /* //! ilman divin keytä tulee error, koska div on arrayn root element*/
         <div key={course.id}>
         <Header course={course}/>
         <Content course={course} key={course.id}/>
@@ -13,20 +12,19 @@ const Course = ({ courses }) => {
     )
   }
   
-  const Header = ({ course }) => {
+const Header = ({ course }) => {
     return (
       <h4>{course.name}</h4>
     )
   }
   
-    const Content = ({ course }) => {
+const Content = ({ course }) => {
     return (
       course.parts.map(part => <Part key={part.id} part={part}/>)
     )
   } 
   
-  const Part = (props) => {  
-    console.log("from component Part, showing", props);
+const Part = (props) => {  
     return (
       <p>
         {props.part.name} {props.part.exercises}
@@ -34,13 +32,8 @@ const Course = ({ courses }) => {
     )
   }
   
-   const Total = ({ course }) => {
-    console.log("from component Total, showing 'props.course'", course);
-    //! arg. a = accumulator eli siihen "kerätään" arvot
-    //! arg. c = currentValue eli kierroksen nykyinen arvo (tässä: course.part)
-    //! "a + c.exercises" on joka kierroksen return value ja samalla koko funktion return value (lopussa)
-    //! 0 on initialValue eli a:n arvo ekalla kierroksella, ilman sitä arrayn indeksi 0 skipataan
-    const sum = course.parts.reduce((a, c) => console.log(c.exercises) || a + c.exercises, 0);
+const Total = ({ course }) => {
+    const sum = course.parts.reduce((a, c) => a + c.exercises, 0);
     return(
       <p> <b>Number of exercises {sum} </b> </p>
     ) 
